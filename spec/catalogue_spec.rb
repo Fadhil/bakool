098e1c146ef8752e3cbca91bd1d74676fb230dd4
Fadhil Luqman <fadhil.luqman@gmail.com>
2025-08-09 14:26:21 +0800

    feat: catalogue

---
+++ spec/catalogue_spec.rb
@@ -0,0 +1,34 @@
+require "catalogue"
+
+describe Catalogue do
+  describe "without any products" do
+    it "initializes with an empty array" do
+      catalogue = Catalogue.new
+      expect(catalogue.products).to eq([])
+    end
+
+    it "can add a product" do
+      catalogue = Catalogue.new
+      catalogue.add_product(Product.new("Product 1", "P1", 100))
+      expect(catalogue.products.first.name).to eq("Product 1")
+      expect(catalogue.products.first.code).to eq("P1")
+      expect(catalogue.products.first.price).to eq(100)
+    end
+  end
+
+  describe "::default_catalogue" do
+    it "returns an array of default products" do
+      catalogue = Catalogue::default_catalogue
+      expect(catalogue.products.count).to eq(3)
+      expect(catalogue.products[0].name).to eq("Red Widget")
+      expect(catalogue.products[0].code).to eq("R01")
+      expect(catalogue.products[0].price).to eq(32.95)
+      expect(catalogue.products[1].name).to eq("Green Widget")
+      expect(catalogue.products[1].code).to eq("G01")
+      expect(catalogue.products[1].price).to eq(24.95)
+      expect(catalogue.products[2].name).to eq("Blue Widget")
+      expect(catalogue.products[2].code).to eq("B01")
+      expect(catalogue.products[2].price).to eq(7.95)
+    end
+  end
+end
